11afcb3be5118132cdc6cbb0c9b736ab98d1ca94
Braulio Manzano <brauliomanzano004@hotmail.com>
2025-07-24 06:43:41 -0600

    actualizacion 1.2.2

--- script.js
+++ script.js
@@ -591,8 +591,6 @@ document.addEventListener('DOMContentLoaded', () => {
         img.onload = function() {
             // Definir tamaño máximo para el puzzle
             const maxPuzzleSize = Math.min(window.innerWidth * 0.95, 420); // <= 340px siempre
-
-            
             // Calcular proporciones
             const ratio = Math.min(1, Math.min(maxPuzzleSize / img.width, maxPuzzleSize / img.height));
             const scaledWidth = img.width * ratio;
