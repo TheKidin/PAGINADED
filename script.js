a1b2d6090b3429711ec72a640f0a2c204fcf5438
Braulio Manzano <brauliomanzano004@hotmail.com>
2025-07-24 06:21:05 -0600

    actualizacion 1.2

--- script.js
+++ script.js
@@ -605,6 +605,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Ajustar el tablero al tamaño calculado
             puzzleBoard.style.width  = `${scaledWidth}px`;
             puzzleBoard.style.height = `${scaledHeight}px`;
+            document.documentElement.style.setProperty('--puzzle-size', `${pieceWidth}px`);
 
             
             // Ajustar tamaño del tablero
