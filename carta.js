0df15919a01287ff37304f6fb0c6d6bb5db11dea
Braulio Manzano <brauliomanzano004@hotmail.com>
2025-07-25 12:19:09 -0600

    actualizacio link discord1.7

--- carta.js
+++ carta.js
@@ -40,8 +40,8 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>`,
          `<div class="discord-link">
     <p>Solo los observadores sabrán a dónde ir...</p>
-    Dale clic al boton rojo si te atrevez
     <a href="https://discord.gg/dedsafioo" class="discord-button" target="_blank">
+        <i class="fa-brands fa-discord"></i> Iniciar acceso
     </a>
 </div>`
 
