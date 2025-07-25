df47e355203cedabcdce70b314d4745f4866abaa
Braulio Manzano <brauliomanzano004@hotmail.com>
2025-07-25 11:32:46 -0600

    actualizacio link discord4

--- carta.js
+++ carta.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <p class="cruel-logo">C.R.U.E.L.</p>
         </div>`,
          `<div class="discord-link">
-         <a href="http://discord.gg/dedsafioo" target="_blank">Ir al Discord</a>
+         <a href="http://discord.gg/dedsafioo" target="_blank">
         <img src="img/icodisc.png" alt="Discord" class="discord-icon">
         </a>
         </div>`
